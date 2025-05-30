c49b6874dbd8a5e4fafddbcc9f802559d90fe4af
Tamara.Lic <tamara.martinezmr@uanl.edu.mx>
2025-05-30 12:49:55 -0600

    laboratorio

---
+++ LABORATORIO/Lab 1_Semana 1_R.R
@@ -0,0 +1,62 @@
+# Tamara Martínez Martínez 
+# 2067694
+# 20/03/2025  - 30/04/2025
+
+# Parte 1 R y Rstudio
+
+# Gastos totales 
+300 + 240 + 1527 + 1833
+celular <- 300
+celular
+
+
+# no puede iniciar con un número 
+# no puede iniciar con guion bajo
+# no puede contener espacios 
+# Es correcto muy largo 
+
+
+# Funciones 
+
+# Valor absoluto (absolute value)
+abs(10)
+abs(-4)
+# Raiz cuadrado (square root)
+sqrt(9)
+# Logaritmo natural (natural logarithm)
+log(2)
+
+
+# Comentarios en R 
+
+# este es un comentario
+# este es otro comentario 
+2 * 9
+4 + 5 # tambien se puede colocar un comentario 
+
+
+# R distingue mayusculas y minusculas 
+# Detectar mayusculas y minusculas 
+celular <- 300
+Celular <- -300
+CELULAR <- 8000
+celular + celular
+
+# Obtener ayuda 
+
+# Documentación acerca de la funcion abs
+help(abs)
+# Documentación de la funcion mean
+help("mean")
+# Documentación acerca de la funcion abs
+?abs
+# Documentación de la funcion mean
+?mean
+# buscar absolute 
+help.search("absolute")
+# Alternativamente 
+??absolute
+
+# Autoevaluación 
+gastos <- c(300, 240, 1527, 400, 1500, 1833)
+barplot(gastos)
